c7355d9d6b7c5053e557d48dd57576c82c44ea2b
KendrickPC <kennethpchang@gmail.com>
2022-04-13 16:30:30 -0700

    Board logic connected with components and states

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-// import logo from './logo.svg';
 import './App.css';
 import Board from './components/Board.js'
 import Keyboard from './components/Keyboard.js'
+import { boardDefault } from './Words';
+import { createContext, useState } from 'react';
+export const AppContext = createContext();
+
 function App() {
+  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{board, setBoard}}>
         <Board />
         <Keyboard />
-      </nav>
+      </AppContext.Provider>
     </div>
   );
 }
